caaa0fd44cf15ce3712d298b13bd0b607a85505a
katie <kjo@live.ca>
2015-07-07 14:50:53 +0200

    Add 2 test files to test function that reduces order of K
    - in laguerre_order: check order of output controller for Laguerre basis with different parameters, generalized basis and user-defined basis
    - in laguerre_loop: loop through different Laguerre parameters and orders and check the order of the controller is correct for each one

---
+++ test_files/laguerre_loop.m
@@ -0,0 +1,47 @@
+%% Script to test function to reduce the order of K when using Laguerre basis functions
+load flex_trans.mat
+s = tf('s');
+z = tf('z');
+
+Ld = 1/(5*s);
+
+n = 10;
+Mm = 0.45;
+Ku = 1;
+wc = 1;
+
+F = z/(z-1);
+Fc = 1/s;
+Gc = d2c(G{1});
+
+per = conper('LS',[Mm,Ku,wc],Ld);
+
+
+%% Continuous
+for n=1:10
+    for a=0:0.1:1
+        phi = conphi('Laguerre',[a, n],'s',Fc);
+        K = condes(Gc,phi,per);
+        Ktf = tf(K);
+        num = Ktf.num{1};
+        if length(num(num~=0)) > n+1
+            fprintf('%4.1f %i %i \n',a,n,length(num(num~=0)));
+            pause
+        end
+    end
+end
+
+
+%% Discrete
+for n=1:10
+    for a=0:0.1:1
+        phi = conphi('Laguerre',[G{1}.Ts, a, n],'z',F);
+        K = condes(G,phi,per);
+        Ktf = tf(K);
+        num = Ktf.num{1};
+        if length(num(num~=0)) > n+1
+            fprintf('%4.1f %i %i \n',a,n,length(num(num~=0)));
+            pause
+        end
+    end
+end
---
+++ test_files/laguerre_order.m
@@ -0,0 +1,47 @@
+load flex_trans.mat
+s = tf('s');
+z = tf('z');
+
+Ld = 1/(5*s);
+
+n = 10;
+Mm = 0.45;
+Ku = 1;
+wc = 1;
+
+F = z/(z-1);
+
+%% Laguerre - 0
+phi = conphi('Laguerre',[G{1}.Ts, 0, n],'z',F);
+per = conper('LS',[Mm,Ku,wc],Ld);
+
+K = condes(G,phi,per);
+tf(K)
+
+%% Laguerre - 1
+phi = conphi('Laguerre',[G{1}.Ts, 0.9, n],'z',F);
+per = conper('LS',[Mm,Ku,wc],Ld);
+
+K = condes(G,phi,per);
+tf(K)
+
+%% Laguerre - 2
+phi = conphi('Laguerre',[G{1}.Ts, 0.6, n],'z',F);
+per = conper('LS',[Mm,Ku,wc],Ld);
+
+K = condes(G,phi,per);
+tf(K)
+
+%% Generalized
+phi=conphi('Generalized',[0.1 0.2 0.3 0.4 0 0.6 0 0 0 0 0 0 0 ],'z',F);
+per = conper('LS',[Mm,Ku,wc],Ld);
+
+K = condes(G,phi,per);
+tf(K)
+
+%% User-defined
+phi=conphi('UD',[1/(z-1);1/(z*(z-1));1/(z^2*(z-1));1/(z^3*(z-1));1/(z^4*(z-1));1/(z^5*(z-1));1/(z^6*(z-1));1/(z^7*(z-1))]);
+per = conper('LS',[Mm,Ku,wc],Ld);
+
+K = condes(G,phi,per);
+tf(K)
