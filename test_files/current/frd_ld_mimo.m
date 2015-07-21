de75c6aac176d430c8ec834e9e35729fe124f23a
KatieUSB <kjo@live.ca>
2015-07-21 09:29:18 +0200

    Add file showing bug for MIMO Ld given as mixed FRD/tf

---
+++ test_files/current/frd_ld_mimo.m
@@ -0,0 +1,29 @@
+clc
+addpath('../../toolbox');
+addpath('../../orig_toolbox');
+
+% Single model, SISO, continuous, PID, 'GPhC', Linprog
+
+s=tf('s');
+G=[exp(-s)/(s+1)^3; exp(-s)/(s+1)^3];
+
+[~,~,w] = bode(G);
+
+Ld = 5/s;
+
+phi=conphi('PID');
+per{1}{1}=conper('LS',0.4,frd(Ld,w));
+per{1}{2}=conper('LS',0.4,Ld);
+
+K=condes(G,phi,per);
+
+% Optimization terminated.
+% 
+% K =
+%  
+%   174 s^2 - 16.5 s + 7.769
+%   ------------------------
+%        s^2 + 83.33 s
+%  
+% Continuous-time transfer function.
+
