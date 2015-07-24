787d942c941cd6d46c42356551ca6420f8eb3b9e
katie <kjo@live.ca>
2015-07-24 21:50:41 +0200

    Add function check_Ld_stability to check if Ld is a stabilizing loop gain for given G and phi
    - in test_files/current
    - ignore cases where G or Ld is FRD model
    - use 10th order Pade approximation when model has internal delays

--- test_files/current/Ld_stability.m
+++ test_files/current/Ld_stability.m
@@ -1,24 +1,15 @@
 s = tf('s');
-% Ld =  100*(s-800)^4/(s*(s-200)^8);
-Ld = 10*(s+18)^7/(s^3*(s+100)^7);
-% Ld = 1/(s^3*(s-1));
+Ld = 10*(s+1)/((s)*(s-1));
 Ldfrd = frd(Ld,logspace(-12,12,100000));
 
-G = 1/(s-1*exp(-s));
-phi = conphi('PID');
+G = 1/(s-1);
+G = zpk(G);
 
-per = {conper('LS',0.4,Ldfrd)};
+Ld = zpk(Ld);
+Ld = ss(Ld);
+Ld.InputDelay = 0.01;
+phi = conphi('Laguerre',[0.5 4],'s',1/s);
 
-[n_un,n_bd] = frd_windingno(Ldfrd);
-
-p = pole(Ld);
-pcl = pole(feedback(1,Ld));
-
-n_un2 = n_un + sum(real(pcl)>0);
-real_n_un = sum(real(p) > 0);
-real_n_bd = sum(real(p) == 0);
-
-n_un2 == real_n_un
-n_bd == real_n_bd
+per = {conper('LS',0.4,Ld)};
 
 check_Ld_stability(per,G,phi.phi);
--- test_files/current/check_Ld_stability.m
+++ test_files/current/check_Ld_stability.m
@@ -6,64 +6,71 @@ end
 for m=1:length(inG)
     G = inG{m};
     Ld = per{m}.Ld;
-    if isdt(phi)
-        Ts = phi.Ts;
-    else
-        Ts = 0;
+    
+    if isa(G,'frd') || isa(Ld,'frd')
+        % if G or Ld are FRD models, skip
+        continue;
     end
-
-    Phi = zpk([],[],0,Ts);
+    
+    % Check closed-loop stability of Ld
+    CLd = feedback(1,Ld);
+    if isdt(CLd)
+        x = isstable(absorbDelay(Cld));
+    elseif isa(CLd,'ss') && ~isempty(CLd.InternalDelay) && CLd.InternalDelay ~= 0
+        x = isstable(pade(CLd,10));
+    else
+        x = isstable(CLd);
+    end
+    if x==0
+        warning('Ld appears to be closed-loop unstable.')
+    end
+    
+    
+    % get poles on stability boundary of Phi
+    Phi = zpk([],[],0,phi.Ts);
     for i=1:size(phi,1)
-        Phi = Phi + zpk([],pole(phi(i)),1,Ts);
+        Phi = Phi + zpk([],pole(phi(i)),1,phi.Ts);
     end
     Phi = minreal(Phi); % lowest common denominator of phi
+    [~,pphi_bd] = getpoles(Phi);
 
-    [~,nphi_bd] = getpoles(Phi);
+    % get poles of G
+    [n_uns,p_bd] = getpoles(G);
+    
+    % combine poles of G and phi and sort
+    p_bd = sort([p_bd; pphi_bd]);
 
-    if isa(G,'frd')
-        n_uns = -1;
-        n_bd = -1;
-    else
-        [n_uns,n_bd] = getpoles(G);
-    end
-
-    n_bd = n_bd + nphi_bd;
-
-    if isa(Ld,'frd')
-        [nL_uns,nL_bd] = frd_windingno(Ld);
-    else
-        [nL_uns,nL_bd] = getpoles(Ld);
-    end
-
-    if n_uns~=-1
-        if n_bd~=nL_bd
-            warning('Ld and G*K should have the same number of poles on the stability boundary, but it appears that Ld has %i and G{%i}*K has %i. This choice of Ld may generate an unstable controller.',nL_bd,m,n_bd)
-        elseif n_uns~=nL_uns
-            warning('Ld and G*K should have the same number of unstable poles, but it appears that Ld has %i and G{%i}*K has %i. This choice of Ld may generate an unstable controller.',nL_uns,m,n_uns)
-        end
-    else
-        fprintf('Cannot evaluate stability of controller for FRD plant models. The Ld given will generate a stable controller for a plant with %i unstable poles and a plant/controller with %i poles on the stability boundary.',nL_uns,nL_bd);
+    % get poles of L
+    [nL_uns,pL_bd] = getpoles(Ld);
+    pL_bd = sort(pL_bd);
+    
+    if length(p_bd)~=length(pL_bd) || sum(p_bd~=pL_bd)~=0
+        warning('Ld and G*K should have the same poles on the stability boundary. This choice of Ld may generate an unstable controller.')
+    elseif n_uns~=nL_uns
+        warning('Ld and G*K should have the same number of unstable poles, but it appears that Ld has %i and G{%i}*K has %i. This choice of Ld may generate an unstable controller.',nL_uns,m,n_uns)
     end
 end
 
 end
 
 
-function [n_uns,n_bd] = getpoles(G)
+function [n_uns,p_bd] = getpoles(G)
+% Function to return the number of unstable poles of sys G and the location
+% of poles on the stability boundary. If model has internal delays, use
+% 10th order Pade approximation to check stability.
 if isdt(G)
-    G = delay2z(G);
-    p = pole(G);
-    n_uns = sum(abs(p)>1);
-    n_bd = sum(abs(p)==1);
+    p = pole(absorbDelay(G));
+    n_uns = sum(abs(p)-1>eps);
+    p_bd = p(abs(abs(p)-1)<eps);
 else
     if isa(G,'ss') && ~isempty(G.InternalDelay) && G.InternalDelay ~= 0
-        disp('Model has internal delays. Using a 10th order Pade approximation to check stability.');
+%         disp('Model has internal delays. Using a 10th order Pade approximation to check stability.');
         p = pole(pade(G,10));
     else
         p = pole(G);
     end
-    n_uns = sum(real(p)>0);
-    n_bd = sum(real(p)==0);
+    n_uns = sum(real(p)>eps);
+    p_bd = p(abs(real(p))< eps);
 end
 end
 
