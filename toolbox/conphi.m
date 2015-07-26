0eca0a01fc585e6ab59f63bd125ae8acac641f92
katie <kjo@live.ca>
2015-07-26 12:17:32 +0200

    Modify conphi to generate phi for state space
    - add file test_conphi.m to test all combinations and check for bugs
    - take 2 extra arguments: ConStruc and ConOpt
    - ConStruc is 'ss' for state space
    - ConOpt gives B/C matrix (optional)
    - eigenvalues of A taken from controller type (PID etc) or manually input in ConPar

--- toolbox/conphi.m
+++ toolbox/conphi.m
@@ -1,4 +1,4 @@
-function phi = conphi(ConType,ConPar,CorD,F)
+function phi = conphi(ConType,ConPar,CorD,F,ConStruc,ConOpt)
 
 % In this command the controller structure is determined.
 %
@@ -29,13 +29,24 @@ function phi = conphi(ConType,ConPar,CorD,F)
 %       [Ts xi].
 %
 %       For user defined structure, ConPar is a column vector of stable
-%       transfer functions.
+%       transfer functions. For state space, ConPar can also be a vector
+%       Aeigs giving the eigenvalues of the A matrix. For discrete time,
+%       ConPar is [Ts Aeigs].
 %
 % CorD : is a string that can be 's' or 'z' to define continuous-time or
 %       discrete-time controller. If it is not assigned a continuous-time 
 %       controller will be aconsidered as default.
 %
 % F: is a transfer function that is fixed in the controller (e.g. an integrator)
+%
+%
+% ConStruc: is a string that can be 'LP' for linearly parameterized
+%       controllers (default) or 'SS' for a state space representation
+%
+% ConOpt: for state space, is a cell with two entries: a string 'B' or 'C'
+%       and a matrix giving the fixed B or C matrix for the controller
+%       (default is C = [1 0 0 ...])
+%
 %
 % Examples:
 %   phi=conphi('PID');  % defines a continuous-time PID controller
@@ -61,12 +72,14 @@ if (nargin < 3)
     CorD='s';
 end
 %-------------------------
-
+if nargin < 5
+    ConStruc = 'LP';
+end
+%-------------------------
 if strcmp(CorD,'s')
     s=zpk('s');
 else
     z=zpk('z');
-
 end
 %-------------------------
 
@@ -75,6 +88,7 @@ if ~ischar (ConType)
 end
 %-------------------------
 ConType = lower ([ConType,'  ']); % removes sensitivity to cases.
+ConStruc = lower(ConStruc);
 %-------------------------
 
 switch ConType(1:3)
@@ -213,10 +227,26 @@ switch ConType(1:3)
         end
         
     case 'ud '
-        [ntoto,n]=size(ConPar);
-        if n >1, error('phi should be a column transfer function vector'); end 
-        phi.phi=ConPar;
-        phi.ConType='ud';
+        if ~isnumeric(ConPar)
+            [ntoto,n]=size(ConPar);
+            if n >1, error('phi should be a column transfer function vector'); end 
+            phi.phi=ConPar;
+            phi.ConType='ud';
+        elseif ~strcmp(ConStruc,'ss')
+            error('phi should be a column transfer function vector');
+        elseif ~isvector(ConPar)
+            error('phi should be a column transfer function vector or a vector of eigenvalues of the A matrix');
+        else
+            phi.phi = [];
+            if strcmp(CorD,'z')
+                Ts = ConPar(1);
+                ConPar = ConPar(2:end);
+            end
+            ns = length(ConPar);
+            a = poly(ConPar);
+            a = a(2:end);            
+            phi.ConType = '';
+        end
         
         
     otherwise
@@ -226,9 +256,124 @@ switch ConType(1:3)
 end
 
 
-if nargin > 3
+if nargin > 3 && ~isempty(F)
     phi.phi=minreal(F*phi.phi);
     phi.ConType =['Ftimes' phi.ConType];
 end
+
+
+
+if strcmp(ConStruc,'ss')
+    phi.par.flag = 0;
+    phi.ConType = 'ss';
+    % Determine eigenvalues
+    if ~isempty(phi.phi)
+        Ts = phi.phi.Ts;
+        if strcmp(CorD,'s') && (strncmp(ConType,'pid',3) || strncmp(ConType,'pd ',3)) && tau==0
+            a = []; % can't determine eigenvalues without derivative time constant
+            ns = size(phi.phi,1)-1;
+            phi.par.flag = 1;
+        else
+            if strncmp(ConType,'p  ',3)
+                phi.ConType = 'p  ';
+                phi.par = [];
+                return; % don't change anything for just proportional control
+            else
+                Phi = zpk([],[],0,Ts);
+                if isa(phi.phi,'tf')
+                    phi.phi = zpk(phi.phi);
+                end
+                for i=1:size(phi.phi,1)
+                    Phi = Phi + zpk([],phi.phi(i).p,1,Ts);
+                end
+                Aeigs = pole(minreal(Phi));
+            end
+            ns = length(Aeigs); % number of states
+            a = poly(Aeigs);
+            a = a(2:end);
+        end
+    end
+    
+    
+    % Determine B or C matrix
+    if nargin < 6
+        C = [1, zeros(1,ns-1)];
+        B = [];
+    elseif strcmpi(ConOpt{1},'b')
+        B = ConOpt{2};
+        if size(B,1) ~= ns
+            error('B matrix should have the same number of columns as A')
+        end
+        C = [];
+    elseif strcmpi(ConOpt{1},'c')
+        B = [];
+        C = ConOpt{2};
+        if size(C,2) ~= ns
+            error('C matrix should have the same number of rows as A')
+        end
+    else
+        error('ConOpt{1} should be a string ''B'' or ''C''');
+    end
+    
+    % Make A matrix from controllable canonical form
+    if length(a)<2
+        A = -a;
+    else
+        A = full(spdiags(ones(ns,1),1,[zeros(ns-1,ns); -flipud(a(:))']));
+        if ~isempty(B) % if B matrix given, use observable canonical
+            A = A';
+        end
+    end
+    
+    
+    if strcmp(CorD,'s')
+        var = zpk('s');
+    else
+        var = zpk('z',Ts);
+    end
+    
+    % Make phi output
+    if ~phi.par.flag
+        if ~isempty(C) % If C matrix given
+            if size(C,1) > 1 % If multiple outputs given
+                phi = cell(size(C,1),1);
+                for i=1:size(C,1)
+                    phi{i,1}.phi = minreal(transpose(C(i,:)/(var*eye(ns)-A)));
+                end
+            else
+                phi.phi = minreal(transpose(C/(var*eye(ns)-A)));
+            end
+        else % If B matrix given
+            if size(B,2) > 1 % if multiple inputs given
+                phi = cell(1,size(B,2));
+                for i=1:size(B,2)
+                    phi{1,i}.phi = minreal((var*eye(ns)-A)\B(:,i));
+                end
+            else
+                phi.phi = minreal((var*eye(ns)-A)\B);
+            end
+        end
+        
+        if iscell(phi)
+            phi{1,1}.par.A = A;
+            phi{1,1}.par.B = B;
+            phi{1,1}.par.C = C;
+            for i=1:length(phi)
+                phi{i}.phi(end+1) = zpk([],[],1,Ts);
+                phi{i}.ConType = 'ss';
+            end
+        else
+            phi.par.A = A;
+            phi.par.B = B;
+            phi.par.C = C;
+            phi.phi(end+1) = zpk([],[],1,Ts);
+            phi.ConType = 'ss';
+        end
+    else
+        phi.par.B = B;
+        phi.par.C = C;
+    end
+end
+
 
 
