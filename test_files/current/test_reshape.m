6c4208410d1510d2a441516a97b1bd71e8b3e2dc
katie <kjo@live.ca>
2015-07-26 20:41:26 +0200

    Add A and f reshaping to test file

--- test_files/current/test_reshape.m
+++ test_files/current/test_reshape.m
@@ -1,12 +1,14 @@
 %%
 
-ni = 3;
+ni = 1;
 no = 2;
 ns = 1;
-Ngs = 1;
+Ngs = 2;
 
 ntot = ni*no*(ns+1)*Ngs;
 nl = (ns+1)*Ngs;
+
+alpha = 4;
 
 H = 2*ones(ntot,ntot);
 H = reshape(H,ntot,nl,no,ni);
@@ -18,8 +20,19 @@ H(:,nl-Ngs+1:end,:,:) = H(:,nl-Ngs+1:end,:,:) + 4;
 H = reshape(H,ntot,ntot);
 H = H';
 
+A = ones(alpha,ntot);
+A = reshape(A,alpha,nl,no,ni);
+A(:,nl-Ngs+1:end,:,:) = A(:,nl-Ngs+1:end,:,:) + 2;
+A = reshape(A,alpha,ntot);
+
+f = ones(ntot,1);
+f = reshape(f,nl,no,ni);
+f(nl-Ngs+1:end,:,:) = f(nl-Ngs+1:end,:,:) + 2;
+f = reshape(f,ntot,1);
+
 B_ss = [];
-%%
+
+%% H reshape
 
 nl = ntot/no/ni; % number of total parameters per input/output
 nbc = nl - Ngs; % number of B/C parameters per input/output
@@ -95,5 +108,50 @@ Hdb = sum(Hdb,ndim);
 Hdb = reshape(Hdb,Ngs*no*ni,nbc*n)';
 
 Hout = [Hbb, Hdb; Hbd, Hdd];
+
+
+%% A reshape
+
+A2 = reshape(A,alpha,nl,no,ni);
+
+Abc = A2(:,1:nbc,:,:);
+Ad = A2(:,nbc+1:end,:,:);
+
+Ad = reshape(Ad,alpha,Ngs*no*ni);
+
+if isempty(B_ss)
+    ndim = 4;
+    n = no;
+else
+    ndim = 3;
+    n = ni;
+end
+
+Ab = sum(Abc,ndim);
+Ab = reshape(Ab,alpha,nbc*n);
+
+Aout = [Ab, Ad];
+
+%% f reshape
+
+f2 = reshape(f,nl,no,ni);
+
+fb = f2(1:nbc,:,:);
+fd = f2(nbc+1:end,:,:);
+
+fd = reshape(fd,Ngs*no*ni,1);
+
+if isempty(B_ss)
+    ndim = 4;
+    n = no;
+else
+    ndim = 3;
+    n = ni;
+end
+
+fb = sum(fb,ndim-1);
+fb = reshape(fb,nbc*n,1);
+
+fout = [fb; fd];
 
 
