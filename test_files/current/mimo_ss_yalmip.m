91c900bb1ea6de49c7a6f5b217baa3dbb53301a1
katie <kjo@live.ca>
2015-07-28 12:33:47 +0200

    Script to loop through performance types, plant dimensions and continuous/discrete to check yalmip vs quadprog results
    - identical except for H-inf (probably because yalmip achieves better gamma)

--- test_files/current/mimo_ss_yalmip.m
+++ test_files/current/mimo_ss_yalmip.m
@@ -1,27 +1,53 @@
 addpath('../../toolbox');
 addpath(genpath('../../../matlab_tools'));
+s = tf('s');
+z = tf('z');
+close all
 
 % Checklist
 % size: 2x2, 2x1, 1x2
 % per: LS, Hinf, GPhC
 % default C, given C, given B
+% discrete, continuous
+continuous = 1;
+nper = 1;
+model = 2;
 
-s = tf('s');
-per = conper('LS',0.3,5/s);
+for continuous=1:2
+    for nper=1:3
+        for model=1:3
 
-W{1} = 1/(s+1);
-% per = conper('hinf',W,1/s);
+switch nper
+    case 1
+        per = conper('LS',0.3,5/s);
+    case 2
+        W{1} = 1/(s+1);
+        per = conper('hinf',W,1/s);
+    case 3
+        per = conper('GPhC',[2 45 10],1/s);
+end
 
-% per = conper('GPhC',[2 45 10],1/s);
-
-G = [2/(s+0.4), 0.001; 2/(s+1), 0.05/(s+2)];
-% G = [2/(s+1); 0.05/(s+2)];
+switch model
+    case 1
+        G = [2/(s+0.4), 0.001; 2/(s+1), 0.05/(s+2)];
+    case 2
+        G = [2/(s+1); 0.05/(s+2)];
+    case 3
+        G = [2/(s+1), 0.05/(s+2)];
+end
 opts = condesopt('gbands','off','yalmip','on','lambda',[1 0 0 0],'gamma',[0.01 2 0.001]);
 opts2 = condesopt('gbands','off','yalmip','off','lambda',[1 0 0 0],'gamma',[0.01 2 0.001]);
 
+Ts = 0.02;
+
+
 %% PID, default C
-phi1 = conphi('pid',0.1,'s',[],'ss');
-phi2 = conphi('pid',0.1,'s');
+
+if continuous
+    phi1 = conphi('pid',0.1,'s',[],'ss');
+else
+    phi1 = conphi('pid',Ts,'z',[],'ss');
+end
 
 K1 = condes(G,phi1,per,opts);
 K2 = condes(G,phi1,per,opts2);
@@ -29,8 +55,11 @@ K2 = condes(G,phi1,per,opts2);
 figure; bode(K1,K2);
 
 %% PID, given C
-phi1 = conphi('pid',0.1,'s',[],'ss',{'c',[7 1]});
-phi2 = conphi('pid',0.1,'s');
+if continuous
+    phi1 = conphi('pid',0.1,'s',[],'ss',{'c',[7 1]});
+else
+    phi1 = conphi('pid',Ts,'z',[],'ss',{'c',[7 1]});
+end
 
 K1 = condes(G,phi1,per,opts);
 K2 = condes(G,phi1,per,opts2);
@@ -38,8 +67,11 @@ K2 = condes(G,phi1,per,opts2);
 figure; bode(K1,K2);
 
 %% PID, given B
-phi1 = conphi('pid',0.1,'s',[],'ss',{'b',[1 1]'});
-phi2 = conphi('pid',0.1,'s');
+if continuous
+    phi1 = conphi('pid',0.1,'s',[],'ss',{'b',[1 1]'});
+else
+    phi1 = conphi('pid',Ts,'z',[],'ss',{'b',[1 1]'});
+end
 
 K1 = condes(G,phi1,per,opts);
 K2 = condes(G,phi1,per,opts2);
@@ -47,17 +79,25 @@ K2 = condes(G,phi1,per,opts2);
 figure; bode(K1,K2);
 
 %% Laguerre, default C
-phi1 = conphi('Laguerre',[0.1 6],'s',[],'ss');
-phi2 = conphi('Laguerre',[0.1 6],'s');
+if continuous
+    phi1 = conphi('Laguerre',[0.1 6],'s',[],'ss');
+else
+    phi1 = conphi('Laguerre',[Ts 0.1 6],'z',[],'ss');
+end
+
 
 K1 = condes(G,phi1,per,opts);
-K2 = condes(G,phi2,per,opts);
+K2 = condes(G,phi1,per,opts2);
 
 figure; bode(K1,K2);
 
 %% Laguerre, given C
-phi1 = conphi('Laguerre',[0.1 5],'s',[],'ss',{'c',[1 2 3 4 5]});
-phi2 = conphi('Laguerre',[0.1 5],'s');
+if continuous
+    phi1 = conphi('Laguerre',[0.1 5],'s',[],'ss',{'c',[1 2 3 4 5]});
+else
+    phi1 = conphi('Laguerre',[Ts 0.1 5],'z',[],'ss',{'c',[1 2 3 4 5]});
+end
+
 
 K1 = condes(G,phi1,per,opts);
 K2 = condes(G,phi1,per,opts2);
@@ -65,12 +105,22 @@ K2 = condes(G,phi1,per,opts2);
 figure; bode(K1,K2);
 
 %% Laguerre, given B
-phi1 = conphi('Laguerre',[0.1 5],'s',[],'ss',{'b',[1 2 3 4 5 ]'});
-phi2 = conphi('Laguerre',[0.1 12],'s');
+if continuous
+    phi1 = conphi('Laguerre',[0.1 5],'s',[],'ss',{'b',[1 2 3 4 5 ]'});
+else
+    phi1 = conphi('Laguerre',[Ts 0.1 5],'z',[],'ss',{'b',[1 2 3 4 5 ]'});
+end
+
 
 K1 = condes(G,phi2,per,opts);
 K2 = condes(G,phi2,per,opts2);
 
 figure; bode(K1,K2);
 
+pause
+close all
+        end
+    end
+    
+end
 
