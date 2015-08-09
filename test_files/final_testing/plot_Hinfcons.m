7abaaa0d841eed067c9d5d4a0f8836f86a91a7c2
katie <kjo@live.ca>
2015-08-09 16:00:58 +0200

    Fix plot function to work with multiple theta for gain scheduled

--- test_files/final_testing/plot_Hinfcons.m
+++ test_files/final_testing/plot_Hinfcons.m
@@ -1,7 +1,12 @@
-function [] = plot_Hinfcons(G,K,W,lambda,gamma,w,theta)
+function [] = plot_Hinfcons(G,K,W,lambda,gamma,w,theta,np)
 
 if ~iscell(G)
     G = {G};
+end
+if ~iscell(w)
+    w2=cell(1,length(G));
+    w2(1,:)={w};
+    w=w2;
 end
 figure(1); figure(2); figure(3); figure(4);
 hold all
@@ -9,30 +14,32 @@ for i=1:length(G)
     if iscell(K)
         K2=K{1};
         for j=1:size(theta,2)
-            K2 = K2 + theta(i,j)*K{j+1};
+            for k=1:np(j)
+                K2 = K2 + theta(i,j)*K{j+1}^(k);
+            end
         end
     else
         K2=K;
     end
-    S = feedback(1,K2*G);
+    S = feedback(1,K2*G{i});
     T = 1-S;
-    U = K*S;
+    U = K2*S;
 
 
     if sum(lambda)==0
-        figure(1); bode(W{1}*S,tf(gamma),w{1})
-        figure(2); bode(W{2}*T,tf(gamma),w{1})
-        figure(3); bode(W{3}*U,tf(gamma),w{1})
+        figure(1); bode(W{i}*S,tf(gamma),w{i})
+        figure(2); bode(W{2}*T,tf(gamma),w{i})
+        figure(3); bode(W{3}*U,tf(gamma),w{i})
     else
-        figure(1); bode(lambda(1)*W{1}*S + lambda(2)*W{2}*T + lambda(3)*W{3}*U,tf(gamma),w{1})
+        figure(1); bode(lambda(1)*W{i}*S + lambda(2)*W{2}*T + lambda(3)*W{3}*U,tf(gamma),w{i})
         if lambda(1)==0
-            figure(2); bode(W{1}*S,tf(1),w{1})
+            figure(2); bode(W{i}*S,tf(1),w{i})
         end
         if lambda(2)==0
-            figure(3); bode(W{2}*T,tf(1),w{1})
+            figure(3); bode(W{2}*T,tf(1),w{i})
         end
         if lambda(3)==0
-            figure(4); bode(W{3}*U,tf(1),w{1})
+            figure(4); bode(W{3}*U,tf(1),w{i})
         end
     end
 end
