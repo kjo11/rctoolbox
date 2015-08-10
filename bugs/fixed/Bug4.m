1b2b2f75680c5d29a03ddb9b1f4a13b6d387f68e
katie <kjo@live.ca>
2015-08-10 16:10:59 +0200

    Remove info.txt

--- bugs/fixed/Bug4.m
+++ bugs/fixed/Bug4.m
@@ -6,6 +6,7 @@ addpath('../../toolbox')
 s = tf('s');
 
 G = [1/(s^2+14*s+7.5); 2/(20*s+1)];
+G = [1/(s^2+14*s+7.5), tf(1); tf(1), 2/(20*s+1)];
 phiLag = conphi('PI');
 per = conper('LS',0.1,5/s);
 
