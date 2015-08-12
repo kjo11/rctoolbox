22573faa248da689bddff3ef0eb1f8ac87f672a0
katie <kjo@live.ca>
2015-08-12 11:15:12 +0200

    Update yalmip test file

--- test_files/current/yalmip_hinf.m
+++ test_files/current/yalmip_hinf.m
@@ -1,5 +1,5 @@
-addpath('../../toolbox')
-addpath(genpath('../../../matlab_tools'))
+addpath('../toolbox')
+addpath(genpath('../../matlab_tools'))
 
 s = tf('s');
 
@@ -8,10 +8,17 @@ tau = 2;
 P = G*exp(-tau*s);
 H = G*(1-exp(-tau*s));
 Ld = 1/s;
-W{1} = tf(0.04);
+W{1} = tf(0.4);
 
 phi = conphi('pid',[],'s',[],'sp',H);
 per = conper('Hinf',W,Ld);
-opts = condesopt('yalmip','on','gamma',[0.2 2 0.01],'lambda',[1 0 0 0],'ntheta',[]);
+opts = condesopt('yalmip','on','gamma',[0.2 1 0.1],'lambda',[1 0 0 0],'ntheta',[]);
 
-K = condes(P,phi,per,opts);
+[C,sol] = condes(P,phi,per,opts);
+
+
+%%
+K = feedback(C,H);
+S = feedback(1,K*P);
+
+figure; bode(W{1}*S,tf(sol.gamma))
