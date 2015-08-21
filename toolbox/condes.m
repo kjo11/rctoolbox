09620c0eb655619873e4e83d25a30880d21b8b0f
katie <kjo@live.ca>
2015-08-21 15:07:31 +0200

    Set nq to 8 when it's empty and YALMIP not found

--- toolbox/condes.m
+++ toolbox/condes.m
@@ -120,6 +120,11 @@ else  % Use optimization toolbox
     YesYalmip=0;
     rho=zeros(ntot,1);
     
+    if isempty(options.nq)
+        warning('Yalmip not found. Setting nq to 8')
+        options.nq=8;
+    end
+    
     ops = optimset;
     if ~isempty (options.solveroptions)
         names = fieldnames(options.solveroptions);
